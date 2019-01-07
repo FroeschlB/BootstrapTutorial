dc58f4dda38c4163c2f16b5d364ada754ac4788c
„FroeschlB“ <froeschl.bernhard@gmail.com>
2019-01-07 12:55:23 +0100

    First commit

---
+++ wp-config.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Grundeinstellungen für WordPress
+ *
+ * Zu diesen Einstellungen gehören:
+ *
+ * * MySQL-Zugangsdaten,
+ * * Tabellenpräfix,
+ * * Sicherheitsschlüssel
+ * * und ABSPATH.
+ *
+ * Mehr Informationen zur wp-config.php gibt es auf der
+ * {@link https://codex.wordpress.org/Editing_wp-config.php wp-config.php editieren}
+ * Seite im Codex. Die Zugangsdaten für die MySQL-Datenbank
+ * bekommst du von deinem Webhoster.
+ *
+ * Diese Datei wird zur Erstellung der wp-config.php verwendet.
+ * Du musst aber dafür nicht das Installationsskript verwenden.
+ * Stattdessen kannst du auch diese Datei als wp-config.php mit
+ * deinen Zugangsdaten für die Datenbank abspeichern.
+ *
+ * @package WordPress
+ */
+
+// ** MySQL-Einstellungen ** //
+/**   Diese Zugangsdaten bekommst du von deinem Webhoster. **/
+
+/**
+ * Ersetze datenbankname_hier_einfuegen
+ * mit dem Namen der Datenbank, die du verwenden möchtest.
+ */
+define('DB_NAME', 'BootstrapTutorial');
+
+/**
+ * Ersetze benutzername_hier_einfuegen
+ * mit deinem MySQL-Datenbank-Benutzernamen.
+ */
+define('DB_USER', 'BootstrapTutorial');
+
+/**
+ * Ersetze passwort_hier_einfuegen mit deinem MySQL-Passwort.
+ */
+define('DB_PASSWORD', 'BootstrapTutorial');
+
+/**
+ * Ersetze localhost mit der MySQL-Serveradresse.
+ */
+define('DB_HOST', 'localhost');
+
+/**
+ * Der Datenbankzeichensatz, der beim Erstellen der
+ * Datenbanktabellen verwendet werden soll
+ */
+define('DB_CHARSET', 'utf8mb4');
+
+/**
+ * Der Collate-Type sollte nicht geändert werden.
+ */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Sicherheitsschlüssel
+ *
+ * Ändere jeden untenstehenden Platzhaltertext in eine beliebige,
+ * möglichst einmalig genutzte Zeichenkette.
+ * Auf der Seite {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * kannst du dir alle Schlüssel generieren lassen.
+ * Du kannst die Schlüssel jederzeit wieder ändern, alle angemeldeten
+ * Benutzer müssen sich danach erneut anmelden.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'K>8f.v!n{XD~JSDi[)Jx>M7q~d[ad?3{hIEd1q*E!#yvqo>nWMQ~$wa9?J}a^n0>');
+define('SECURE_AUTH_KEY',  '>ZYhRGQ6Ml3DJ=bvD;paxAZ5SuT=,;MS`Q+JEC43f[4Io;[7>e`*!M-kLqe>vZOT');
+define('LOGGED_IN_KEY',    '-[as4f^.6hYQU]ekPV^~~M>5U&l@C{5dNVvay$A5|2[fZ;zy1:5/:WTe}X:q9R*7');
+define('NONCE_KEY',        'lGI<*}{:3GC+G<ykczlo=9R.r3OV2e34.f!nyg)Yt5{_m*sKKn4ukd}ECA7#G9O*');
+define('AUTH_SALT',        'y7+_OS^zW4i^J@R5Ok;S Lv0xqNf0ZE*5BRTTB]@@Iop>*++9?QoE77wz1<] W/Z');
+define('SECURE_AUTH_SALT', 'mK^8(-LP_PGu.%q!bsRl 0e`l.Ma2%po=l^>r9CUG(Maw0Z6l<<{,)SB[O}O@;2e');
+define('LOGGED_IN_SALT',   'dDB4:e:CR1,|CCt<Ej^N#tVy-<k/ 5NL<9Ef-]r:>MFe|T:X[Q]>Pg>yC6Pk|CO7');
+define('NONCE_SALT',       '&^2_[2e7BG3g#Lf{cw5XW)Rj?zTUV<N|GS<Ie: U@5;xS(7K^TK4f?4#lzP0% }k');
+
+/**#@-*/
+
+/**
+ * WordPress Datenbanktabellen-Präfix
+ *
+ * Wenn du verschiedene Präfixe benutzt, kannst du innerhalb einer Datenbank
+ * verschiedene WordPress-Installationen betreiben.
+ * Bitte verwende nur Zahlen, Buchstaben und Unterstriche!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * Für Entwickler: Der WordPress-Debug-Modus.
+ *
+ * Setze den Wert auf „true“, um bei der Entwicklung Warnungen und Fehler-Meldungen angezeigt zu bekommen.
+ * Plugin- und Theme-Entwicklern wird nachdrücklich empfohlen, WP_DEBUG
+ * in ihrer Entwicklungsumgebung zu verwenden.
+ *
+ * Besuche den Codex, um mehr Informationen über andere Konstanten zu finden,
+ * die zum Debuggen genutzt werden können.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+/* Das war’s, Schluss mit dem Bearbeiten! Viel Spaß beim Bloggen. */
+/* That's all, stop editing! Happy blogging. */
+
+/** Der absolute Pfad zum WordPress-Verzeichnis. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Definiert WordPress-Variablen und fügt Dateien ein.  */
+require_once(ABSPATH . 'wp-settings.php');
